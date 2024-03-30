727b803db4ab74d414a398705c2d4456a3f74936
おかーかじぇーぴー <okakajp.25@gmail.com>
2024-03-30 08:48:05 +0000

    a

---
+++ my-next/app/api-response/page.js
@@ -0,0 +1,11 @@
+"use client"
+import React from "react";
+import MainComponent from "./c";
+
+export default function api() {
+    return (
+        <main className="flex min-h-screen items-center flex-col p-24">
+           <MainComponent/> 
+        </main>
+    )
+}
